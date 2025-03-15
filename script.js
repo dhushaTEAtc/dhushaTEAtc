dc0345ed26347a70789c92b6c9ca6586165d1a27
dhushaTEAtc <rajathuraimichael549@gmail.com>
2025-03-16 04:20:21 +0530

    Add files via upload
    Download .Zip File
    Extract .Zip File
    TAP on "index.html"!

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle button interaction (for "Learn More" functionality)
+document.getElementById('learnMore').addEventListener('click', function() {
+    alert("Tissue culture involves growing plant cells or tissues under sterile conditions. More information coming soon!");
+});
